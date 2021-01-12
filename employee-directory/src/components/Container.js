64f728d7ab7f0aa6a93b4858706639e0c41df421
timsilby <69242373+timsilby@users.noreply.github.com>
2021-01-12 14:12:38 +1000

    Added table.

--- employee-directory/src/components/Container.js
+++ employee-directory/src/components/Container.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import SearchBar from './FilterForm';
+import Table from './Table';
 
 const Container = () => {
 	return (
 		<section className="container bg-secondary">
 			<SearchBar/>
+			<Table/>
 		</section>
 	);
 }
---
+++ employee-directory/src/components/Table.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Table = () => {
+	return (
+		<table className="table table-dark table-striped table-hover">
+			<thead>
+				<tr>
+					<th scope="col">Image</th>
+					<th scope="col">Name</th>
+					<th scope="col">Phone</th>
+					<th scope="col">Email</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>Mark</td>
+					<td>Otto</td>
+					<td>@mdo</td>
+				</tr>
+				<tr>
+					<td>Jacob</td>
+					<td>Thornton</td>
+					<td>@fat</td>
+				</tr>
+				<tr>
+					<td colspan="2">Larry the Bird</td>
+					<td>@twitter</td>
+				</tr>
+			</tbody>
+		</table>
+	);
+}
+
+export default Table;
