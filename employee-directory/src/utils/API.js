f71e691cfce399f5c899209202dbea08af03a495
timsilby <69242373+timsilby@users.noreply.github.com>
2021-01-12 14:25:36 +1000

    Added function to fetch users from api.

---
+++ employee-directory/src/utils/API.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const fetchUsers = () => {
+	return axios.get("https://randomuser.me/api/?results=20&format=json&inc=picture,name,email,phone&noinfo");
+}
+
+export default fetchUsers;
